1e7d159cefbb41a8df21f6972327dabc6ebbeca3
Miguel García Reimers <mgarciareimers@gmail.com>
2024-07-24 23:00:49 +0200

    04-gif-expert-app: Title + Input + Dummy list created.

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 const GifExpertApp = () => {
+    const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+    // Add a category to the list.
+    const addCategory = (category) => {
+        setCategories([ category, ...categories ]);
+    };
+
     return (
         <>
             { /* Title */ }
             <h1>GifExpertApp</h1>
 
             { /* Input */ }
+            <AddCategory 
+                addCategory= { addCategory }
+            />
 
             { /* List */ }
+            <ol>
+               { categories.map((category) => {
+                return <li key={ category }>{ category }</li>
+               })} 
+            </ol>
         </>
     );
 }
---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export const AddCategory = ({ addCategory }) => {
+    const [ value, setValue ] = useState('');
+
+    // Update value.
+    const onValueChanged = (event) => {
+        setValue(event.target.value);
+    }
+
+    // Form submit has been clicked.    
+    const onSubmitClicked = (event) => {
+        event.preventDefault();
+
+        if (value.trim().length <= 0) {
+            return;
+        }
+
+        addCategory(value.trim());
+        setValue('');
+    }
+
+    return (
+        <form onSubmit={ onSubmitClicked }>
+            <input 
+                type='text'
+                placeholder='Search gifs'
+                value={ value }
+                onChange={ onValueChanged }
+            />
+        </form>
+    );
+}
