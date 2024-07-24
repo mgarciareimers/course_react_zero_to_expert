bbea8cfd99491f330bc36b64f967bc81746f3271
Miguel García Reimers <mgarciareimers@gmail.com>
2024-07-24 19:43:59 +0200

    Start of project.

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <>
+            { /* Title */ }
+            <h1>GifExpertApp</h1>
+
+            { /* Input */ }
+
+            { /* List */ }
+        </>
+    );
+}
+
+export default GifExpertApp;
