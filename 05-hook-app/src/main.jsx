d28df518a99051fde9820dfa91456cc3d1a4e898
Miguel García Reimers <mgarciareimers@gmail.com>
2024-08-04 18:48:00 +0200

    05-hook-app useRef hook implementation finished.

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,10 +6,11 @@ import './index.css'
 // import CounterApp from './01-useState/CounterApp'
 // import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
 // import SimpleFormWithCustomHook from './02-useEffect/SimpleFormWithCustomHook'
-import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
+// import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
+import FocusScreen from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>,
 )
