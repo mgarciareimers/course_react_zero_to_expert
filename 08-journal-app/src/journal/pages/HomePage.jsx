a67f892a2432a67a7ddd0bc46740fe22a239498a
Miguel García Reimers <mgarciareimers@gmail.com>
2024-09-08 22:00:22 +0200

    08-journal-app: Basi UI of project developed.

--- 08-journal-app/src/journal/pages/HomePage.jsx
+++ 08-journal-app/src/journal/pages/HomePage.jsx
@@ -1,13 +1,31 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import React from 'react';
 import JournalLayout from '../layouts/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 const HomePage = () => {
     return (
         <JournalLayout>
-            { /* <NothingSelectedView /> */ }
-            <NoteView />
+            <NothingSelectedView /> 
+            { /* <NoteView />*/ }
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': {
+                        backgroundColor: 'error.main',
+                        opacity: 0.9
+                    },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
         </JournalLayout>
     );
 }
